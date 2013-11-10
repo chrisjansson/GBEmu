436a55e92ddc2754bda589882aca109bd4072663
Chris Jansson <chris.janssom@gmail.com>
2013-11-10 10:24:31 +0100

    LD (nn), A

--- source/Core/Cpu.cs
+++ source/Core/Cpu.cs
@@ -120,6 +120,7 @@ namespace Core
             { 0x7E, new InstructionMetaData(1, 2, "LD A, (HL)")},
             { 0x7F, new InstructionMetaData(1, 1, "LD A, A")},
             { 0xC3, new InstructionMetaData(0, 4, "JP, nn")},
+            { 0xEA, new InstructionMetaData(3, 4, "LD (nn), A")},
             { 0xF3, new InstructionMetaData(1, 1, "DI")},
         };
 
@@ -412,6 +413,11 @@ namespace Core
                     var l = _mmu.GetByte((ushort)(ProgramCounter + 1));
                     var h = _mmu.GetByte((ushort)(ProgramCounter + 2));
                     ProgramCounter = (ushort)((h << 8) | l);
+                    break;
+                case 0xEA:
+                    _mmu.SetByte(
+                        (ushort) ((_mmu.GetByte((ushort) (ProgramCounter + 2)) << 8) | _mmu.GetByte((ushort) (ProgramCounter + 1))),
+                        A);
                     break;
                 case 0xF3:
                     break;
---
+++ source/Test/LD_nn_A.cs
@@ -0,0 +1,33 @@
+﻿using Core;
+using Xunit;
+
+namespace Test
+{
+    public class LD_nn_A : IUseFixture<CpuFixture>
+    {
+        private Cpu _cpu;
+        private FakeMmu _fakeMmu;
+
+        [Fact]
+        public void Loads_a_into_nn()
+        {
+            _cpu.A = 0xAB;
+            _cpu.ProgramCounter = 10189;
+            _cpu.Cycles = 92181;
+            _fakeMmu.SetByte(10190, 0xAC);
+            _fakeMmu.SetByte(10191, 0x18);
+
+            _cpu.Execute(0xEA);
+
+            Assert.Equal(0xAB, _fakeMmu.GetByte(0x18AC));
+            Assert.Equal(10192, _cpu.ProgramCounter);
+            Assert.Equal(92185, _cpu.Cycles);
+        }
+
+        public void SetFixture(CpuFixture data)
+        {
+            _cpu = data.Cpu;
+            _fakeMmu = data.FakeMmu;
+        }
+    }
+}
