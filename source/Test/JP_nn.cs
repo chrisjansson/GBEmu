acb504173dfc74ed475c75b78da9c09a49a9e0d6
Chris Jansson <chris.janssom@gmail.com>
2013-12-01 09:53:24 +0100

    Clean up

--- source/Test/JP_nn.cs
+++ source/Test/JP_nn.cs
@@ -3,39 +3,30 @@ using Xunit;
 
 namespace Test
 {
-    public class JP_nn : IUseFixture<CpuFixture>
+    public class JP_nn : CpuTestBase
     {
-        private Cpu _sut;
-        private FakeMmu _fakeMmu;
-
         [Fact]
         public void Loads_nn_into_program_counter_lower_order_byte_first()
         {
-            _sut.ProgramCounter = 9876;
+            Cpu.ProgramCounter = 9876;
             const byte l = 123;
             const byte h = 234;
-            _fakeMmu.SetByte(9877, l);
-            _fakeMmu.SetByte(9878, h);
+            FakeMmu.SetByte(9877, l);
+            FakeMmu.SetByte(9878, h);
 
-            _sut.Execute(0xC3);
+            Cpu.Execute(0xC3);
 
-            Assert.Equal(60027, _sut.ProgramCounter);
+            Assert.Equal(60027, Cpu.ProgramCounter);
         }
 
         [Fact]
         public void Advances_clock()
         {
-            _sut.Cycles = 7931;
+            Cpu.Cycles = 7931;
 
-            _sut.Execute(0xC3);
+            Cpu.Execute(0xC3);
 
-            Assert.Equal(_sut.Cycles, 7935);
-        }
-
-        public void SetFixture(CpuFixture fixture)
-        {
-            _sut = fixture.Cpu;
-            _fakeMmu = fixture.FakeMmu;
+            Assert.Equal(Cpu.Cycles, 7935);
         }
     }
 }
