c3b770bcbc82312ab45a3ad4faf6f5503f62414c
Chris Jansson <chris.janssom@gmail.com>
2014-02-18 19:46:15 +0100

    Rewrote CP_n to use cputestbase

--- source/Test/CP_n.cs
+++ source/Test/CP_n.cs
@@ -4,55 +4,40 @@ using Xunit;
 
 namespace Test
 {
-    public class CP_n
+    public class CP_n : CpuTestBase
     {
-        private Cpu _cpu;
-        private FakeMmu _fakeMmu;
-
-        public CP_n()
-        {
-            var cpuFixture = new CpuFixture();
-            _cpu = cpuFixture.Cpu;
-            _fakeMmu = cpuFixture.FakeMmu;
-        }
-
         [Fact]
         public void Program_counter_cycles_sets_N()
         {
-            _cpu.N = 0;
-            _cpu.ProgramCounter = 9281;
-            _cpu.Cycles = 81123;
+            Flags(x => x.ResetSubtract());
 
-            _cpu.Execute(0xFE);
+            Execute(CreateOpCode());
 
-            Assert.Equal(1, _cpu.N);
-            Assert.Equal(9283, _cpu.ProgramCounter);
-            Assert.Equal(81125, _cpu.Cycles);
+            AssertFlags(x => x.SetSubtract());
+            AdvancedClock(2);
+            AdvancedProgramCounter(2);
         }
 
         [Fact]
         public void Sets_z_when_A_is_equal_to_n()
         {
-            _cpu.A = 0xAB;
-            _cpu.ProgramCounter = 9281;
-            _fakeMmu.SetByte(9282, 0xAB);
+            Flags(x => x.ResetZero());
+            Cpu.A = 0xAB;
 
-            _cpu.Execute(0xFE);
+            Execute(CreateOpCode(), 0xAB);
 
-            Assert.Equal(1, _cpu.Z);
+            AssertFlags(x => x.SetZero());
         }
 
         [Fact]
         public void Resets_z_when_A_is_not_equal_to_n()
         {
-            _cpu.Z = 1;
-            _cpu.A = 0x34;
-            _cpu.ProgramCounter = 3621;
-            _fakeMmu.SetByte(3622, 0xAB);
+            Flags(x => x.Zero());
+            Cpu.A = 0x34;
 
-            _cpu.Execute(0xFE);
+            Execute(CreateOpCode(), 0xAB);
 
-            Assert.Equal(0, _cpu.Z);
+            AssertFlags(x => x.ResetZero());
         }
 
         [Fact]
@@ -60,6 +45,11 @@ namespace Test
         {
             //A - n is the order of subtraction
             throw new NotImplementedException();
+        }
+
+        private byte CreateOpCode()
+        {
+            return 0xFE;
         }
     }
 }
