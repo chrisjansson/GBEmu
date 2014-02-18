1f2feb555165e63ed6e276723b55f29c4a0c1e5b
Chris Jansson <chris.janssom@gmail.com>
2014-02-18 20:13:44 +0100

    Extracted tests into a template base class

---
+++ source/Test/CPTestBase.cs
@@ -0,0 +1,65 @@
+﻿using Xunit;
+
+namespace Test
+{
+    public abstract class CPTestBase : CpuTestBase
+    {
+        protected abstract void ExecuteCompareATo(byte value);
+
+        [Fact]
+        public void Program_counter_cycles_sets_N()
+        {
+            Flags(x => x.ResetSubtract());
+
+            ExecuteCompareATo(0xAB);
+
+            AssertFlags(x => x.SetSubtract());
+            AdvancedClock(2);
+            AdvancedProgramCounter(2);
+        }
+
+        [Fact]
+        public void Sets_z_when_A_is_equal_to_n()
+        {
+            Flags(x => x.ResetZero());
+            Cpu.A = 0xAB;
+
+            ExecuteCompareATo(0xAB);
+
+            AssertFlags(x => x.SetZero());
+        }
+
+        [Fact]
+        public void Resets_z_when_A_is_not_equal_to_n()
+        {
+            Flags(x => x.Zero());
+            Cpu.A = 0x34;
+
+            ExecuteCompareATo(0xAB);
+
+            AssertFlags(x => x.ResetZero());
+        }
+
+        [Fact]
+        public void Sets_carry()
+        {
+            Flags(x => x.ResetCarry());
+            Cpu.A = 0x3C;
+
+            ExecuteCompareATo(0x40);
+
+            AssertFlags(x => x.SetCarry().ResetHalfCarry());
+        }
+
+        [Fact]
+        public void Sets_half_carry_when_borrowing_from_4th_to_3rd_bit()
+        {
+            Flags(x => x.ResetHalfCarry());
+            Cpu.A = 0x3C;
+
+            ExecuteCompareATo(0x2F);
+
+            AssertFlags(x => x.SetHalfCarry().ResetCarry());
+        }
+    }
+}
--- source/Test/CP_n.cs
+++ source/Test/CP_n.cs
@@ -1,63 +1,10 @@
-﻿using Xunit;
-
-namespace Test
+﻿namespace Test
 {
-    public class CP_n : CpuTestBase
+    public class CP_n : CPTestBase
     {
-        [Fact]
-        public void Program_counter_cycles_sets_N()
+        protected override void ExecuteCompareATo(byte value)
         {
-            Flags(x => x.ResetSubtract());
-
-            Execute(CreateOpCode());
-
-            AssertFlags(x => x.SetSubtract());
-            AdvancedClock(2);
-            AdvancedProgramCounter(2);
-        }
-
-        [Fact]
-        public void Sets_z_when_A_is_equal_to_n()
-        {
-            Flags(x => x.ResetZero());
-            Cpu.A = 0xAB;
-
-            Execute(CreateOpCode(), 0xAB);
-
-            AssertFlags(x => x.SetZero());
-        }
-
-        [Fact]
-        public void Resets_z_when_A_is_not_equal_to_n()
-        {
-            Flags(x => x.Zero());
-            Cpu.A = 0x34;
-
-            Execute(CreateOpCode(), 0xAB);
-
-            AssertFlags(x => x.ResetZero());
-        }
-
-        [Fact]
-        public void Sets_carry()
-        {
-            Flags(x => x.ResetCarry());
-            Cpu.A = 0x3C;
-
-            Execute(CreateOpCode(), 0x40);
-
-            AssertFlags(x => x.SetCarry().ResetHalfCarry());
-        }
-
-        [Fact]
-        public void Sets_half_carry_when_borrowing_from_4th_to_3rd_bit()
-        {
-            Flags(x => x.ResetHalfCarry());
-            Cpu.A = 0x3C;
-
-            Execute(CreateOpCode(), 0x2F);
-
-            AssertFlags(x => x.SetHalfCarry().ResetCarry());
+            Execute(CreateOpCode(), value);
         }
 
         private byte CreateOpCode()
