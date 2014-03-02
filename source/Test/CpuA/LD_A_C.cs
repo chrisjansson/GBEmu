77e6483b64bf941e79d2d0a3cac239cc99d1e74e
Chris Jansson <chris.janssom@gmail.com>
2014-03-02 10:38:53 +0100

    LD A, (C)

---
+++ source/Test/CpuA/LD_A_C.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Test.CpuA
+{
+    public class LD_A_C : CpuTestBase
+    {
+        private const byte OpCode = 0xF2;
+
+        [Fact]
+        public void Advances_counters()
+        {
+            Execute(OpCode);
+
+            AdvancedProgramCounter(1);
+            AdvancedClock(2);
+        }
+
+        [Fact]
+        public void Loads_FF00_plus_C_into_a()
+        {
+            FakeMmu.Memory[0xFFAC] = 0xCE;
+            Cpu.C = 0xAC;
+
+            Execute(OpCode);
+
+            Assert.Equal(0xCE, Cpu.A);
+        }
+    }
+}
