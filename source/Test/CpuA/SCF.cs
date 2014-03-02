549f6dc6f13933c5ad56d594d5e58f680eeaebe7
Chris Jansson <chris.janssom@gmail.com>
2014-03-02 10:35:39 +0100

    SCF tests

---
+++ source/Test/CpuA/SCF.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace Test.CpuA
+{
+    public class SCF : CpuTestBase
+    {
+        private const byte OpCode = 0x37;
+
+        [Fact]
+        public void Advances_counters()
+        {
+            Execute(OpCode);
+
+            AdvancedProgramCounter(1);
+            AdvancedClock(1);
+        }
+
+        [Fact]
+        public void Sets_carry()
+        {
+            Flags(x => x.ResetCarry());
+
+            Execute(OpCode);
+
+            AssertFlags(x => x.SetCarry());
+        }
+
+        [Fact]
+        public void Resets_subtract_and_half_carry()
+        {
+            Flags(x => x.HalfCarry().Subtract());
+
+            Execute(OpCode);
+
+            AssertFlags(x => x.ResetHalfCarry().ResetSubtract());
+        }
+    }
+}
