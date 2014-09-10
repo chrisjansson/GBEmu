9ce79cc19c8f4f215f60fc3261035cfc4648751c
Chris Jansson <chris.janssom@gmail.com>
2014-09-10 17:55:34 +0200

    Reminder to test interrupt priorities when more interrupts are supported

---
+++ source/Test/CpuA/Interrupts/InterruptPriorityTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Test.CpuA.Interrupts
+{
+    public class InterruptPriorityTests
+    {
+        [Fact]
+        public void Reminder_to_test_interrupt_priority()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
