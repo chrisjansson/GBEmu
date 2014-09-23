649fb10ed8a98f4941286ba650f075d58040e0d8
Chris Jansson <chris.janssom@gmail.com>
2014-09-23 19:26:44 +0200

    Cleanup

---
+++ source/Core/RegisterAddresses.cs
@@ -0,0 +1,16 @@
+﻿namespace Core
+{
+    public class RegisterAddresses
+    {
+        public const ushort TAC = 0xFF07; //Timer control
+        public const ushort TMA = 0xFF06; //Timer modulo
+        public const ushort TIMA = 0xFF05; //Timer counter
+        public const ushort LCDC = 0xFF40;
+        public const ushort ScrollX = 0xFF43;
+        public const ushort ScrollY = 0xFF42;
+        public const ushort LY = 0xFF44;
+        public const ushort BGP = 0xFF47;
+        public const ushort IE = 0xFFFF; //Interrupt enable
+        public const ushort IF = 0xFF0F; //Interrupt request
+    }
+}
