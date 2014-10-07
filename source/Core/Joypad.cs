ad8e8ff604077f8f6f645cd68c9caffbda110aad
Chris Jansson <chris.janssom@gmail.com>
2014-10-07 19:50:18 +0200

    First test of joypad

---
+++ source/Core/Joypad.cs
@@ -0,0 +1,12 @@
+﻿namespace Core
+{
+    public class Joypad : IJoypad
+    {
+        private byte _p1;
+        public byte P1
+        {
+            get { return _p1; }
+            set { _p1 = (byte) (value & 0xF0); }
+        }
+    }
+}
---
+++ source/Test/JoypadTests/LowerHalfOfP1IsReadOnly.cs
@@ -0,0 +1,23 @@
+﻿using Core;
+using Xunit;
+
+namespace Test.JoypadTests
+{
+    public class LowerHalfOfP1IsReadOnly
+    {
+        private readonly Joypad _sut;
+
+        public LowerHalfOfP1IsReadOnly()
+        {
+            _sut = new Joypad();
+        }
+
+        [Fact]
+        public void Lower_half_of_JOYP_is_read_only()
+        {
+            _sut.P1 = 0xFF;
+
+            Assert.Equal(_sut.P1, 0xF0);
+        }
+    }
+}
