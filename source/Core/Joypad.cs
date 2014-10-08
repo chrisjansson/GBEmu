8fc0f2a3e285700383476696142bd177585abfc3
Chris Jansson <chris.janssom@gmail.com>
2014-10-08 20:47:50 +0200

    Select button groups

--- source/Core/Joypad.cs
+++ source/Core/Joypad.cs
@@ -8,20 +8,34 @@
             get
             {
                 var buttons = 0x00;
-                buttons = buttons | (Right ? 0x01 : 0x00);
-                buttons = buttons | (Left ? 0x02 : 0x00);
-                buttons = buttons | (Up ? 0x04 : 0x00);
-                buttons = buttons | (Down ? 0x08 : 0x00);
+                if ((_p1 & 0x20) == 0x20)
+                {
+                    buttons = buttons | (A ? 0x01 : 0x00);
+                    buttons = buttons | (B ? 0x02 : 0x00);
+                    buttons = buttons | (Select ? 0x04 : 0x00);
+                    buttons = buttons | (Start ? 0x08 : 0x00);
+                }
+                else
+                {
+                    buttons = buttons | (Right ? 0x01 : 0x00);
+                    buttons = buttons | (Left ? 0x02 : 0x00);
+                    buttons = buttons | (Up ? 0x04 : 0x00);
+                    buttons = buttons | (Down ? 0x08 : 0x00);
+                }
                 int result = _p1;
                 result = result | (~buttons & 0x0F);
-                return (byte) result;
+                return (byte)result;
             }
-            set { _p1 = (byte) (value & 0xF0); }
+            set { _p1 = (byte)(value & 0xF0); }
         }
 
         public bool Right { get; set; }
         public bool Left { get; set; }
         public bool Up { get; set; }
         public bool Down { get; set; }
+        public bool A { get; set; }
+        public bool B { get; set; }
+        public bool Select { get; set; }
+        public bool Start { get; set; }
     }
 }
---
+++ source/Test/JoypadTests/JoypadButtonKeysTests.cs
@@ -0,0 +1,66 @@
+﻿using Core;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Test.JoypadTests
+{
+    public class JoypadButtonKeysTests
+    {
+        private Joypad _sut;
+
+        public JoypadButtonKeysTests()
+        {
+            _sut = new Joypad();
+            _sut.P1 = 0x20; //Select button keys
+        }
+
+        [Theory]
+        [InlineData(false, (byte)0x2F)]
+        [InlineData(true, (byte)0x2E)]
+        public void Bit_0_is_button_A(bool isPressed, byte expectedP1)
+        {
+            _sut.A = isPressed;
+
+            Assert.Equal(expectedP1, _sut.P1);
+        }
+
+        [Theory]
+        [InlineData(false, (byte)0x2F)]
+        [InlineData(true, (byte)0x2D)]
+        public void Bit_1_is_button_B(bool isPressed, byte expectedP1)
+        {
+            _sut.B = isPressed;
+
+            Assert.Equal(expectedP1, _sut.P1);
+        }
+
+        [Theory]
+        [InlineData(false, (byte)0x2F)]
+        [InlineData(true, (byte)0x2B)]
+        public void Bit_2_is_select_button(bool isPressed, byte expectedP1)
+        {
+            _sut.Select = isPressed;
+
+            Assert.Equal(expectedP1, _sut.P1);
+        }
+
+        [Theory]
+        [InlineData(false, (byte)0x2F)]
+        [InlineData(true, (byte)0x27)]
+        public void Bit_3_is_start_button(bool isPressed, byte expectedP1)
+        {
+            _sut.Start = isPressed;
+
+            Assert.Equal(expectedP1, _sut.P1);
+        }
+
+        [Fact]
+        public void Combines_buttons_into_P1()
+        {
+            _sut.Start = true;
+            _sut.B = true;
+
+            Assert.Equal(0x25, _sut.P1);
+        }
+    }
+}
