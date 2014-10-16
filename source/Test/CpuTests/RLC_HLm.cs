3d84c929ee9327fe4123342d8b4b1d46f9835dcc
Chris Jansson <chris.janssom@gmail.com>
2014-10-16 18:00:43 +0200

    RLC (HL)
    wip test

---
+++ source/Test/CpuTests/RLC_HLm.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Test.CpuTests
+{
+    public class RLC_HLm : CpuTestBase
+    {
+        private const byte OpCode = 0x06;
+
+        [Fact]
+        public void Advances_counters()
+        {
+            ExecutingCB(OpCode);
+
+            AdvancedProgramCounter(2);
+            AdvancedClock(4);
+        }
+    }
+}
