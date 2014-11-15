7ce7c8733c04dfff387b54cf563067c516e0ad99
Chris Jansson <chris.janssom@gmail.com>
2014-11-15 10:29:59 +0100

    Failing test for 0x9C00 tile map

--- source/Test/Display/DataTransfer_TileDataSelect8000.cs
+++ source/Test/Display/DataTransfer_TileDataSelect8000.cs
@@ -160,6 +160,24 @@ namespace Test.Display
         }
     }
 
+    public class DataTransfer_TileMapSelect9C00 : DataTransferTestBase
+    {
+        public DataTransfer_TileMapSelect9C00()
+        {
+            _fakeMmu.SetByte(RegisterAddresses.LCDC, 0x18);
+        }
+
+        protected override void InsertTile(byte tileNumber, byte[] tileData)
+        {
+            InsertTileAt((ushort)(0x8000 + tileNumber * 16), tileData);
+        }
+
+        protected override void SetBlockTile(int block, byte tile)
+        {
+            _fakeMmu.Memory[0x9C00 + block] = tile;
+        }
+    }
+
     public static class MMUExtensions
     {
         public static void ScrollX(this IMmu mmu, byte scrollY)
