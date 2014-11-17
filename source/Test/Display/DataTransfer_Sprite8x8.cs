29009cc4582c72b77a6365d1deb8855b016df249
Chris Jansson <chris.janssom@gmail.com>
2014-11-17 20:40:08 +0100

    Failing sprite draw test

--- source/Test/Display/DataTransfer_Sprite8x8.cs
+++ source/Test/Display/DataTransfer_Sprite8x8.cs
@@ -1,7 +1,40 @@
-﻿namespace Test.Display
+﻿using Xunit;
+
+namespace Test.Display
 {
     public class DataTransfer_Sprite8x8 : DataTransferTestBase
     {
+        public DataTransfer_Sprite8x8()
+        {
+            InsertSpriteAttribute(1, new byte[]
+            {
+                0x16, //y = 16, displaycoordinate + 16
+                0x08, //x = 8, displaycoordinate + 8
+                0x00, //Tile number
+                0x00, //Flags
+            });
+        }
+
+        [Fact]
+        public void Draws_sprite_at_origin()
+        {
+            _sut.FinishFrame();
+            _sut.TransferScanLine(0);
+            _sut.TransferScanLine(1);
+
+            var line = GetLine(0);
+            AssertLine(line, FirstTileFirstRow);
+        }
+
+        private void InsertSpriteAttribute(int number, byte[] o)
+        {
+            const int spriteAttributeStartAddress = 0xFE00;
+            for (var i = 0; i < o.Length; i++)
+            {
+                _fakeMmu.Memory[spriteAttributeStartAddress + number * 4 + i] = o[i];
+            }
+        }
+
         protected override void InsertTile(byte tileNumber, byte[] tileData)
         {
             InsertTileAt((ushort)(0x8000 + tileNumber * 16), tileData);
@@ -9,7 +42,6 @@
 
         protected override void SetBlockTile(int block, byte tile)
         {
-            _fakeMmu.Memory[0x9800 + block] = tile;
         }
     }
 }
