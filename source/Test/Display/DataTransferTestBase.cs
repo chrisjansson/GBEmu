09080d7abd94d8156d8bcf6ffcb8fbf317ed1f09
Chris Jansson <chris.janssom@gmail.com>
2014-11-17 19:21:26 +0100

    Refactored display test base so that expected line is stored in a variable

--- source/Test/Display/DataTransferTestBase.cs
+++ source/Test/Display/DataTransferTestBase.cs
@@ -9,6 +9,9 @@ namespace Test.Display
         protected FakeMmu _fakeMmu;
         protected DisplayDataTransferService _sut;
 
+        protected byte[] FirstTileFirstRow;
+        protected byte[] SecondTileFirstRow;
+
         protected DataTransferTestBase()
         {
             _fakeMmu = new FakeMmu();
@@ -43,6 +46,16 @@ namespace Test.Display
             SetBlockTile(0x1F, 128);
             SetBlockTile(0x20, 128);
             SetBlockTile(0x3E0, 128);
+
+            FirstTileFirstRow = new byte[]
+            {
+                0, 3, 3, 3, 3, 3, 0, 0
+            };
+
+            SecondTileFirstRow = new byte[]
+            {
+                0, 0, 3, 3, 3, 3, 0, 0
+            };
         }
 
         protected abstract void InsertTile(byte tileNumber, byte[] tileData);
@@ -53,6 +66,17 @@ namespace Test.Display
             for (var i = 0; i < colors.Length; i++)
             {
                 Assert.Equal(colors[i], line[i]);
+            }
+        }
+
+        protected void AssertLine(byte[] line, params byte[][] colors)
+        {
+            for (var i = 0; i < colors.Length; i++)
+            {
+                for (var j = 0; j < colors[i].Length; j++)
+                {
+                    Assert.Equal(colors[i][j], line[j + i * 8]);
+                }
             }
         }
 
@@ -79,6 +103,7 @@ namespace Test.Display
 
     public abstract class BackgroundTestBase : DataTransferTestBase
     {
+
         [Fact]
         public void Copies_pixels_from_first_and_second_tile_on_first_row()
         {
@@ -87,8 +112,8 @@ namespace Test.Display
 
             var line = GetLine(0);
             AssertLine(line,
-                0, 3, 3, 3, 3, 3, 0, 0, //First tile row 0
-                0, 0, 3, 3, 3, 3, 0, 0); //Second tile row 0
+                FirstTileFirstRow,
+                SecondTileFirstRow);
         }
 
         [Fact]
