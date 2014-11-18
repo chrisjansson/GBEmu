55eb651abb974921650da8d23a0504a276dc41c7
Chris Jansson <chris.janssom@gmail.com>
2014-11-18 17:10:09 +0100

    draw sprites at their x and y coordinate

--- source/Core/IDisplayDataTransferService.cs
+++ source/Core/IDisplayDataTransferService.cs
@@ -59,19 +59,20 @@ namespace Core
                 var spriteAddress = (ushort)(0xFE00 + sprite * 4);
                 var spriteY = _mmu.GetByte(spriteAddress);
                 var displayY = spriteY - 16;
-                if (displayY != line)
-                    continue;
-
-                var spriteX = _mmu.GetByte((ushort)(spriteAddress + 1));
-                var displayX = spriteX - 8;
-
-                var tileNumber = _mmu.GetByte((ushort)(spriteAddress + 2));
-                var tile = _tiles[tileNumber];
-
-                for (var x = 0; x < 8; x++)
+                var spriteYCoord = line - displayY;
+                if (spriteYCoord >= 0 && spriteYCoord <= 7)
                 {
-                    var color = tile.Pixels[x + line * 8];
-                    FrameBuffer[line * WindowWidth + x] = color;
+                    var spriteX = _mmu.GetByte((ushort)(spriteAddress + 1));
+                    var displayX = spriteX - 8;
+
+                    var tileNumber = _mmu.GetByte((ushort)(spriteAddress + 2));
+                    var tile = _tiles[tileNumber];
+
+                    for (var x = 0; x < 8; x++)
+                    {
+                        var color = tile.Pixels[x + spriteYCoord * 8];
+                        FrameBuffer[line * WindowWidth + displayX + x] = color;
+                    }
                 }
             }
         }
--- source/Test/Display/DataTransfer_Sprite8x8.cs
+++ source/Test/Display/DataTransfer_Sprite8x8.cs
@@ -8,6 +8,21 @@ namespace Test.Display
         public DataTransfer_Sprite8x8()
         {
             _fakeMmu.Memory[RegisterAddresses.LCDC] = 0x10;
+            for (int i = 0; i < 40; i++)
+            {
+                InsertSpriteAttribute(i, new byte[]
+                {
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00
+                });
+            }
+        }
+
+        [Fact]
+        public void Draws_sprite_at_origin()
+        {
             InsertSpriteAttribute(1, new byte[]
             {
                 0x10, //y = 16, displaycoordinate + 16
@@ -15,19 +30,55 @@ namespace Test.Display
                 0x00, //Tile number
                 0x00, //Flags
             });
-        }
-
-        [Fact]
-        public void Draws_sprite_at_origin()
-        {
             _sut.FinishFrame();
             _sut.TransferScanLine(0);
+            _sut.TransferScanLine(1);
             _sut.TransferScanLine(7);
 
             var first = GetLine(0);
-            var second = GetLine(7);
+            var second = GetLine(1);
             AssertLine(first, FirstTileFirstRow);
-            //AssertLine(second, 2, 2, 0, 0, 0, 2, 2, 0);
+            AssertLine(second, 2, 2, 0, 0, 0, 2, 2, 0);
+        }
+
+        [Fact]
+        public void Draws_sprite_offet_by_y()
+        {
+            InsertSpriteAttribute(1, new byte[]
+            {
+                0x11, //y = 17, displaycoordinate + 16
+                0x08, //x = 8, displaycoordinate + 8
+                0x00, //Tile number
+                0x00, //Flags
+            });
+            _sut.FinishFrame();
+            _sut.TransferScanLine(0);
+            _sut.TransferScanLine(1);
+
+            var first = GetLine(0);
+            var second = GetLine(1);
+            AssertLine(first, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 });
+            AssertLine(second, FirstTileFirstRow);
+        }
+
+        [Fact]
+        public void Draws_sprite_offet_by_x()
+        {
+            InsertSpriteAttribute(1, new byte[]
+            {
+                0x10, //y = 17, displaycoordinate + 16
+                0x09, //x = 8, displaycoordinate + 8
+                0x00, //Tile number
+                0x00, //Flags
+            });
+            _sut.FinishFrame();
+            _sut.TransferScanLine(0);
+            _sut.TransferScanLine(1);
+
+            var first = GetLine(0);
+            var second = GetLine(1);
+            AssertLine(first, 0, 0, 3, 3, 3, 3, 3, 0, 0);
+            AssertLine(second, 0, 2, 2, 0, 0, 0, 2, 2, 0);
         }
 
         private void InsertSpriteAttribute(int number, byte[] o)
