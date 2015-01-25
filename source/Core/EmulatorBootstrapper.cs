7787dd2b8f98ee5b27c67f18b9b3c876b6bae870
Chris Jansson <chris.janssom@gmail.com>
2015-01-25 19:00:28 +0100

    Added ringbuffer to to log the last sequence of instructions so that they can be printed when the emulator crashes

--- source/Core/EmulatorBootstrapper.cs
+++ source/Core/EmulatorBootstrapper.cs
@@ -4,6 +4,8 @@ namespace Core
 {
     public class Emulator
     {
+        public readonly RingBuffer<Tuple<ushort, byte>> Trace = new RingBuffer<Tuple<ushort, byte>>(50000);
+
         public IMmu Mmu { get; set; }
         public Cpu Cpu { get; set; }
         public Timer Timer { get; set; }
@@ -17,6 +19,7 @@ namespace Core
             var instruction = Mmu.GetByte(next);
 
             var old = Cpu.Cycles;
+            Trace.Insert(new Tuple<ushort, byte>(Cpu.ProgramCounter, instruction));
             Cpu.Execute(instruction);
             var delta = Cpu.Cycles - old;
             for (var i = 0; i < delta; i++)
