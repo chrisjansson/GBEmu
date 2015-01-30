12f2dfca3622694ac3ad86d474c3040b78ddc26e
Chris Jansson <chris.janssom@gmail.com>
2015-01-30 20:14:40 +0100

    Load MBC1 cartridge

--- source/Core/CartridgeLoader.cs
+++ source/Core/CartridgeLoader.cs
@@ -1,10 +1,22 @@
-﻿namespace Core
+﻿using System;
+
+namespace Core
 {
     public class CartridgeLoader
     {
         public IMBC Load(byte[] rom)
         {
-            throw new System.NotImplementedException();
+            var headerBytes = new byte[0x4F];
+            Array.Copy(rom, 0x100, headerBytes, 0, headerBytes.Length);
+            var header = new CartridgeHeaderParser().Parse(headerBytes);
+
+            switch (header.MBC)
+            {
+                case CartridgeHeader.CartridgeTypeEnum.MBC1RAMBATTERY:
+                    return new MBC1(rom, header.ROMSize, header.RAMSize);
+            }
+
+            return null;
         }
     }
 }
